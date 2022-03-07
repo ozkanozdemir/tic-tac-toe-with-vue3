9ac8b3e4663437bddf78d8081bbae1dac62d3132
ozkan <ozkan.ozdemir4114@gmail.com>
2022-03-07 18:14:22 +0300

    updated vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,5 @@ const path = require("path");
 
 module.exports = {
 	outputDir: path.resolve(__dirname, "./docs"),
-	publicPath: 'https://ozkanozdemir.github.io/tic-tac-toe-with-vue3/',
+	publicPath: process.env.NODE_ENV === 'production' ? 'https://ozkanozdemir.github.io/tic-tac-toe-with-vue3/' : '',
 }
